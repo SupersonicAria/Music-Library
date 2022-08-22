05413d45a2e72a6b037f7a1cd5105b549b49c315
unknown <tavoywalls@gmail.com>
2022-08-22 17:32:34 -0400

    added router

--- music_library/src/App.js
+++ music_library/src/App.js
@@ -1,6 +1,11 @@
-import { useEffect, useState } from 'react';
-import SearchBar from './components/SearchBar';
-import Gallery from './components/Gallery';
+
+import { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Gallery from './components/Gallery'
+import SearchBar from './components/SearchBar'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
+import { Fragment } from 'react'
 
 function App() {
   let [search, setSearch] = useState('')
@@ -10,19 +15,19 @@ function App() {
   const API_URL = 'https://itunes.apple.com/search?term='
 
   useEffect(() => {
-    if(search) {
-    const fetchData = async () => {
-      document.title = `${search} Music`
-      const response = await fetch(API_URL + search)
-      const resData = await response.json()
-      if (resData.results.length > 0) {
-        setData(resData.results)
-      } else {
-        setMessage ('Not Found')
+    if (search) {
+      const fetchData = async () => {
+        document.title = `${search} Music`
+        const response = await fetch(API_URL + search)
+        const resData = await response.json()
+        if (resData.results.length > 0) {
+          return setData(resData.results)
+        } else {
+          return setMessage('Not Found')
+        }
       }
+      fetchData()
     }
-    fetchData()
-  }
   }, [search])
 
   const handleSearch = (e, term) => {
@@ -32,9 +37,19 @@ function App() {
 
   return (
     <div>
-      <SearchBar handleSearch = {handleSearch} />
       {message}
-      <Gallery data={data} />
+      <Router>
+        <Routes>
+          <Route path="/" element={
+            <Fragment>
+              <SearchBar handleSearch={handleSearch} />
+              <Gallery data={data} />
+            </Fragment>
+          } />
+          <Route path="/album/:id" element={<AlbumView />} />
+          <Route path="/artist/:id" element={<ArtistView />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- music_library/src/components/SearchBar.js
+++ music_library/src/components/SearchBar.js
@@ -6,9 +6,12 @@ function SearchBar(props) {
     return (
         <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
 
-            <input type='text' placeholder='Enter a search term here'
-                onChange={(e) => props.handleSearch(e.target.value)} />
+            <input type="text" placeholder="Enter a search term here" onChange={
+                (e) => setSearchTerm(e.target.value)
+            } />
+
             <input type="submit" />
+
         </form>
     )
 }
